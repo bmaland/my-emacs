8858c487a42cd2e79a77187703f5bae6c0fa0653
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-01-07 22:49:53 +0100

    Added shell-current-directory from ELPA

---
+++ elpa/shell-current-directory-0.1/shell-current-directory-autoloads.el
@@ -0,0 +1,29 @@
+;;; shell-current-directory-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (shell-current-directory) "shell-current-directory"
+;;;;;;  "shell-current-directory.el" (18788 63399))
+;;; Generated autoloads from shell-current-directory.el
+
+(autoload 'shell-current-directory "shell-current-directory" "\
+Create a shell pertaining to the current directory.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("shell-current-directory-pkg.el") (18788
+;;;;;;  63399 586281))
+
+;;;***
+
+(provide 'shell-current-directory-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; shell-current-directory-autoloads.el ends here
