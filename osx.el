156c1b798cac81e1084b7007700fd6bd77880bd0
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-01-08 14:51:27 +0100

    Updated osx settings, removed the separate file for aquamacs

--- osx.el
+++ osx.el
@@ -9,6 +9,8 @@
                             (mouse-color . "LightSkyBlue")
                             (left . 0) (width . 141) (height . 44)))
 
+(setq browse-url-browser-function 'browse-url-default-macosx-browser)
+
 ;;(define-key dired-mode-map "o" 'dired-open-mac)
 (defun dired-open-mac ()
        (interactive)
@@ -16,6 +18,13 @@
          (if (file-exists-p file-name)
              (call-process "/usr/bin/open" nil 0 nil file-name))))
 
+;; Implementation specific settings
 (if (boundp 'aquamacs-version)
-    (load "~/.emacs.d/aquamacs.el")
+    ;; Aquamacs
+    (progn
+      (one-buffer-one-frame-mode 0)
+      (emulate-mac-finnish-keyboard-mode t)
+      (setq x-select-enable-clipboard t) ;; merge the Emacs kill-ring with the clipboard
+      (set-default-font "-apple-monaco-medium-r-normal--14-120-72-72-m-120-iso10646-1"))
+  ;; Carbon Emacs
   (setq mac-option-modifier 'meta))
