1e15424e5e1bbf8ad8ded37688a777aea3a49845
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-05-04 08:51:00 +0200

    reftex

--- my-hooks.el
+++ my-hooks.el
@@ -141,6 +141,14 @@
           '(lambda ()
              (set (make-local-variable 'scroll-margin) 0)))
 
+(defun org-mode-reftex-setup ()
+  (load-library "reftex")
+  (and (buffer-file-name)
+       (file-exists-p (buffer-file-name))
+       (reftex-parse-all))
+  (define-key org-mode-map (kbd "C-c )") 'reftex-citation))
+(add-hook 'org-mode-hook 'org-mode-reftex-setup)
+
 (add-hook 'org-mode-hook
           (lambda ()
             (require 'bbdb)
