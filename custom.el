2d27257530b932eb2ccaea9e1e76fae1a9b64844
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-16 13:21:33 +0200

    Custom mumamo faces

--- custom.el
+++ custom.el
@@ -50,4 +50,5 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background dark)) (:background "#111111"))))
+ '(mumamo-background-chunk-submode ((((class color) (min-colors 88) (background dark)) (:background "black")))))
