5a119fca24dfa9a1c9d60eb558156b66cc441136
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-09-11 14:33:33 +0200

    Path update

--- hosts/sleep.el
+++ hosts/sleep.el
@@ -1,6 +1,6 @@
 ;; Clojure
-(setq swank-clojure-jar-path "/home/bjorn/foss/clojure/clojure.jar")
-(setq swank-clojure-extra-classpaths '("/home/bjorn/foss/clojure-contrib/src"))
+(setq swank-clojure-jar-path "/home/bjorn/java/clojure.jar")
+(setq swank-clojure-extra-classpaths '("/home/bjorn/java"))
 ;; yasnippet dev
 (setq snippet-dir "~/src/snippets")
 (yas/load-directory snippet-dir)
