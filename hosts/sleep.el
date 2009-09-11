1a33056c4b9febcc30dabac7c875d2efb2599f7c
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-09-11 14:35:13 +0200

    Imaxima

--- hosts/sleep.el
+++ hosts/sleep.el
@@ -6,3 +6,15 @@
 (yas/load-directory snippet-dir)
 ;; etc
 (setq browse-url-generic-program "chromium-browser")
+
+
+;; Imaxima
+(push "/home/bjorn/src/imaxima-imath-1.0" load-path)
+
+;;; add autoload of imaxima and maxima.
+(autoload 'imaxima "imaxima" "Frontend for maxima with Image support" t)
+(autoload 'maxima "maxima" "Frontend for maxima" t)
+;;; add autoload of imath.
+(autoload 'imath-mode "imath" "Imath mode for math formula input" t)
+
+(setq imaxima-fnt-size "Large")
